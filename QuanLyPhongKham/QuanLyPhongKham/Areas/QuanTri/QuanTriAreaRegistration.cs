9cbc866fe078bd72c483ee4719ed2a10f0b594ec
Phương Trang <phuon@DESKTOP-48I7K7A>
2018-03-13 21:34:24 +0700

    Tạo layout share

---
+++ QuanLyPhongKham/QuanLyPhongKham/Areas/QuanTri/QuanTriAreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace QuanLyPhongKham.Areas.QuanTri
+{
+    public class QuanTriAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "QuanTri";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+                "QuanTri_default",
+                "QuanTri/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
